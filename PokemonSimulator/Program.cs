c441391ea07091e7f93554a8efdc25eff24fb12a
srosy <serosenvall@gmail.com>
2020-06-04 20:22:54 -0600

    init base console app. Documents for proposals one and two added. .gitIgnore added.

---
+++ PokemonSimulator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Pokemon World!");
+        }
+    }
+}
