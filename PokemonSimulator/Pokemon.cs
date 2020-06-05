df2accb473fd213bba34d25bcab25c99e1f04a16
Samuel Gardner <ubfunkeyboy@gmail.com>
2020-06-04 21:01:30 -0600

    Initial creation of the Pokemon object class.

---
+++ PokemonSimulator/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonSimulator
+{
+    struct Pokemon
+    {
+        public static Pokemon Factory(string apiRequest)
+        {
+            //do stuff.
+            return new Pokemon();
+        }
+        private Pokemon(/*todo*/)
+        {
+            //do stuff.
+        }
+    }
+}
