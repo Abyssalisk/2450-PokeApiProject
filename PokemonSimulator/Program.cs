461c2cd88a92d89e5bbe8b933a1ed37100aa9a97
Samuel Gardner <ubfunkeyboy@gmail.com>
2020-06-08 00:13:01 -0600

    Doing more MySql Stuff

--- PokemonSimulator/Program.cs
+++ PokemonSimulator/Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace PokemonSimulator
 {
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
+            const string connectionString = "";
+            MySqlConnection server = new MySqlConnection(connectionString);
+            server.Open();
+            string userID = Console.ReadLine().Trim();
+            MySqlCommand command = new MySqlCommand($"SELECT USER_ID FROM USERS WHERE USERS.USER_ID = {userID}", server);
+            MySqlDataReader reader = command.ExecuteReader();
+            //command.CommandText = $"SELECT * {reader.GetFieldValue<int>(0)}";
+
             APIPokemonBlueprint apip = APIPokemonBlueprint.GetPokemonBlueprint("crobat");
             Console.WriteLine(apip.ToString().Length + "\n\n\n");
             Pokemon p = new Pokemon(apip);
