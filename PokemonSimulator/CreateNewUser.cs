8afe0bd418f5a6590ee71104df4e53ddb34f91bc
DrechslerDerek <drechsler.derek@yahoo.com>
2020-06-08 22:50:30 -0600

    branch

--- PokemonSimulator/CreateNewUser.cs
+++ PokemonSimulator/CreateNewUser.cs
@@ -11,21 +11,67 @@ namespace PokemonSimulator
 {
     public class CreateNewUser
     {
+        //Class fields
         string TrainerName;
         string password;
+        string email;
 
         public CreateNewUser(MySqlConnection con)
         {
-            Console.WriteLine("Enter new desired trainer name");
-            TrainerName = Console.ReadLine();
+            //Database only take VARCHAR(100) to save on space, user inputs need to be less than 100 chars
+            while (true)
+            {
+                //Makes sure new user name is less than 100 chars
+                Console.WriteLine("Enter new desired trainer name");
+                TrainerName = Console.ReadLine();
+                if (TrainerName.Length > 99)
+                { 
+                    Console.WriteLine("User Name is to long, enter a shorter one!");
+                }else
+                {
+                    break;
+                }
+            }
 
-            Console.WriteLine("Enter new password");
-            password = Console.ReadLine();
+            while (true)
+            { 
+                //Makes sure new user name is less than 100 chars
+                Console.WriteLine("Enter new password");
+                password = Console.ReadLine();
+                if(password.Length>99)
+                {
+                    Console.WriteLine("Password is to long, enter a shorter one!");
+                }else
+                {
+                    break;
+                }
+            }
+
+            while (true)
+            {
+                //Makes sure new email is less than 100 chars
+                Console.WriteLine("Enter email address");
+                email = Console.ReadLine();
+                if (password.Length > 99)
+                {
+                    Console.WriteLine("Email is to long, choose a different one!");
+                }
+                else
+                {
+                    if (emailValidation(email) == false)
+                    {
+                        Console.WriteLine("Email is invalid!Try again");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
             password = UserPasswordHash(password);
-
-            insertDBcredentials(TrainerName, password, con);
+            insertDBcredentials(TrainerName, password, email, con);
         }
-
+        //This is a private helper method that uses hashing alg to hash the nwe password
         private string UserPasswordHash(string thePass)
         {
             string Hashedpass;
@@ -34,10 +80,22 @@ namespace PokemonSimulator
             return Hashedpass;
         }
 
-        private void insertDBcredentials(String name, String passAfterItHashed, MySqlConnection connection)
+        //Private method to verify email is in the correct form
+        private Boolean emailValidation(string email)
         {
+            string emailRegex;
+            return false;
+        }
+
+        //This method inserts the user login credentials into the DB
+        private void insertDBcredentials(String name, String passAfterItHashed,  String email, MySqlConnection connection)
+        {
+            //Opens a new connection to MySql DB
             connection.Open();
-            string plainTextQuery = "INSERT INTO sql3346222.userCredentials(TrainerName, password) VALUES('" + name + "','" + passAfterItHashed + "');";
+            //INSERT query
+            string plainTextQuery = "INSERT INTO sql3346222.userCredentials(TrainerName, password, email) VALUES('" + name + 
+                "','" + passAfterItHashed + "','"+ email+ "');";
+            //execute the query
             MySqlCommand query = new MySqlCommand(plainTextQuery, connection);
             MySqlDataReader rdr = query.ExecuteReader();
 
--- PokemonSimulator/UserAuthandLogin.cs
+++ PokemonSimulator/UserAuthandLogin.cs
@@ -12,35 +12,68 @@ namespace PokemonSimulator
 
     public class ConsoleOutputInput
     {
+        DBconnect connection = new DBconnect();
+        string loginName = "";
+        string password = "";
         public ConsoleOutputInput()
         {
             Console.WriteLine("Welcome to Pokemon Battle Simulator Console Version!");
-            var connection = new DBconnect();
+
             string newUser;
 
             Console.WriteLine("Are you a new trainer? (y/n)");
             newUser = Console.ReadLine();
-            if(newUser == "y"||newUser == "Y")
+
+            while (true)
             {
-                var createNewTrainer = new CreateNewUser(connection.myConnection);
+                //Choice if user is new, takes them to create user
+                if (newUser == "y" || newUser == "Y")
+                {
+                    var createNewTrainer = new CreateNewUser(connection.myConnection);
+                    Login();//once new user is made pompts for login
+                    break;
+                }
+
+                //Choice if user enters, N not a new user, prompts login
+                if (newUser == "n" || newUser == "N")
+                {
+                    Login();
+                    break;
+                }
+
+                //if something other than y or n is entered user is prompted with choice again
+                Console.WriteLine("Invalid choice! Please type y or n");
+                Console.WriteLine("Are you a new trainer? (y/n)");
+                newUser = Console.ReadLine();
             }
+        }
+
+        //Method to log user in, validates by looking up hashed password
+        private void Login()
+        {
             Console.WriteLine("Please login! Trainer name: ");
-            var loginName = Console.ReadLine();
+            loginName = Console.ReadLine();
 
             Console.WriteLine("enter your password: ");
-            var password = Console.ReadLine();
+            password = Console.ReadLine();
 
+            //validates password
             Validate(loginName, password, connection.myConnection);
         }
+
+        //Private method to validate password, if password is false gives option to reset password
         private void Validate(string userName, string pass, MySqlConnection con)
         {
             string lookupByName = "SELECT password FROM sql3346222.userCredentials WHERE(TrainerName = '"+userName+"');";
             string correctPassword = "";
+            string resetPasswrodYorN = "";
 
+            //opens new DB connection with MySql and pulls hashed password from userCredentials table
             con.Open();
             MySqlCommand query = new MySqlCommand(lookupByName, con);
             MySqlDataReader rdr = query.ExecuteReader();
 
+            //reading returned query
             while (rdr.Read())
             {
                 correctPassword=rdr[0].ToString();
@@ -50,12 +83,37 @@ namespace PokemonSimulator
             string attemptedPassword;
             var sendToHashPasswordAlg = new HashingAlg(pass);
             attemptedPassword = sendToHashPasswordAlg.getHash();
+
+            //checks the hashed password the user entered agaisnt the hashedpass from DB
             if (correctPassword==attemptedPassword)
             {
                 Console.WriteLine("Welcome "+userName);
+            }else //failed login attempt
+            {
+                Console.WriteLine("Username or password incorrect! Please try again!");
+                Console.WriteLine("Do you need to reset your password? (y/n)");
+                resetPasswrodYorN = Console.ReadLine();
+
+                while(true)
+                {
+                    if(resetPasswrodYorN=="y"|| resetPasswrodYorN=="Y")
+                    {
+                        var reset = new ResetPassword();
+                        Login();
+                        break;
+                    }
+                    if(resetPasswrodYorN=="n"|| resetPasswrodYorN=="N")
+                    {
+                        Login();
+                        break;
+                    }
+
+                    Console.WriteLine("Invalid choice, please eneter y to reset password or n to reattempt login!");
+                }
             }
 
         }
+
     }
 
 
