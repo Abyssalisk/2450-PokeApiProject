94544ea308a872c8f3a0252aad7f2e138dafe38d
DrechslerDerek <drechsler.derek@yahoo.com>
2020-06-09 22:11:04 -0600

    revisions made, ready to merge

---
+++ PokemonSimulator/CreateNewUser.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using RestSharp.Validation;
+using System.Text.RegularExpressions;
+
+namespace PokemonSimulator
+{
+    public class CreateNewUser
+    {
+        //Class fields
+        string TrainerName;
+        string password;
+        string email;
+
+        public CreateNewUser(MySqlConnection con)
+        {
+            //Database only take VARCHAR(100) to save on space, user inputs need to be less than 100 chars
+            while (true)
+            {
+                //Makes sure new user name is less than 100 chars
+                Console.WriteLine("Enter new desired trainer name");
+                TrainerName = Console.ReadLine();
+                if (TrainerName.Length > 50)
+                { 
+                    Console.WriteLine("Trainer name is to long, enter a shorter one!");
+                }else
+                {
+                    if (userNameValidation(TrainerName,con) == false)
+                    {
+                        Console.WriteLine("Trainer name is already taken!Try again");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            while (true)
+            { 
+                //Makes sure new user name is less than 100 chars
+                Console.WriteLine("Enter new password");
+                password = Console.ReadLine();
+                if(password.Length>50)
+                {
+                    Console.WriteLine("Password is to long, enter a shorter one!");
+                }else
+                {
+                    break;
+                }
+            }
+
+            while (true)
+            {
+                //Makes sure new email is less than 100 chars
+                Console.WriteLine("Enter email address");
+                email = Console.ReadLine();
+                if (password.Length > 99)
+                {
+                    Console.WriteLine("Email is to long, choose a different one!");
+                }
+                else
+                {
+                    var emailSetup = new EmailValidation(email);
+                    //validates if the entered email is in supported format
+                    if (emailSetup.emailIsInCorrectForm == false)
+                    {
+                        Console.WriteLine("Email is in invalid form!Try again");
+                    }
+                    if (emailSetup.emailIsValid == false)
+                    {
+                        Console.WriteLine("Email could not be validated! Try again");
+                    }else
+                    {
+                        break;
+                    }
+                }
+            }
+            password = UserPasswordHash(password);
+            insertDBcredentials(TrainerName, password, email, con);
+        }
+        //This is a private helper method that uses hashing alg to hash the nwe password
+        private string UserPasswordHash(string thePass)
+        {
+            string Hashedpass;
+            var sendToHashPasswordAlg = new HashingAlg(thePass);
+            Hashedpass = sendToHashPasswordAlg.getHash();
+            Hashedpass = sendToHashPasswordAlg.addSecret(Hashedpass);
+            return Hashedpass;
+        }
+
+        //Private method to verify email is in the correct form
+
+        //checks to see if username is already taken
+        private Boolean userNameValidation(string userName, MySqlConnection con)
+        {
+            con.Open();
+            //INSERT query
+            string plainTextQuery = "SELECT TrainerName FROM sql3346222.userCredentials WHERE(TrainerName = '"+userName+"');";
+            string returnedQuery = "";
+            //execute the query
+            MySqlCommand query = new MySqlCommand(plainTextQuery, con);
+            MySqlDataReader rdr = query.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                returnedQuery=(rdr[0]).ToString();
+            }
+            rdr.Close();
+            con.Close();
+
+            if(returnedQuery==userName)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        //This method inserts the user login credentials into the DB
+        private void insertDBcredentials(String name, String passAfterItHashed,  String email, MySqlConnection connection)
+        {
+            //Opens a new connection to MySql DB
+            connection.Open();
+            //INSERT query
+            string plainTextQuery = "INSERT INTO sql3346222.userCredentials(TrainerName, Password, Email) VALUES('" + name + 
+                "','" + passAfterItHashed + "','"+ email+ "');";
+            //execute the query
+            MySqlCommand query = new MySqlCommand(plainTextQuery, connection);
+            MySqlDataReader rdr = query.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+            }
+            rdr.Close();
+            connection.Close();
+
+            Console.WriteLine("new trainer added!");
+
+        }
+
+    }
+}
---
+++ PokemonSimulator/ResetPassword.cs
@@ -0,0 +1,125 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PokemonSimulator
+{
+    class ResetPassword
+    {
+        string verificationString;
+        string trainerName;
+        string enteredEmail;
+        MySqlConnection connection;
+
+        public ResetPassword(MySqlConnection con)
+        {
+            while (true)
+            {
+                connection = con;
+
+                Console.WriteLine("lets reset your password\nFirst enter your trainer name: ");
+                trainerName = Console.ReadLine();
+
+                string lookupEmailByName = "SELECT email FROM sql3346222.userCredentials WHERE(TrainerName = '" + trainerName + "');";
+                string returnedEmail = "0";
+
+                Console.WriteLine("Enter the email attached to your account: ");
+                enteredEmail = Console.ReadLine();
+
+                con.Open();
+                MySqlCommand query = new MySqlCommand(lookupEmailByName, con);
+                MySqlDataReader rdr = query.ExecuteReader();
+
+                //reading returned query
+                while (rdr.Read())
+                {
+                    returnedEmail = rdr[0].ToString();
+                }
+                rdr.Close();
+                con.Close();
+
+                //if an email is returned from DB this is skipped
+                if (returnedEmail.Length <= 1)
+                {
+                    string newUser;
+                    Console.WriteLine("No email found or user name incorrect!\nPlease try again or create new user");
+                    Console.WriteLine("Would you like to make a new account?(y/n)");
+                    newUser = Console.ReadLine();
+                    while (true)
+                    {
+                        //Choice if user is new, takes them to create user
+                        if (newUser.ToLower().Equals("y"))
+                        {
+                            var backToMakeNewAccount = new ConsoleOutputInput();
+                            break;
+                        }
+
+                        //Choice if user enters, N not a new user, prompts login
+                        if (newUser.ToLower().Equals("n"))
+                        {
+                            break;
+                        }
+
+                        //if something other than y or n is entered user is prompted with choice again
+                        Console.WriteLine("Invalid choice! Please type y or n");
+                        Console.WriteLine("Make new account? (y/n)");
+                        newUser = Console.ReadLine();
+                    }
+                }
+
+                while (true)
+                {
+                    if (returnedEmail == enteredEmail)
+                    {
+                        var emailVerificationForReset = new EmailValidation(returnedEmail);
+                        if(emailVerificationForReset.emailIsValid==true)
+                        {
+                            Console.WriteLine("Lets reset your password...");
+                            makeNewPassword();
+                            var backToLogin = new ConsoleOutputInput();
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Emails do not match! Let's try this again");
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+
+        private void makeNewPassword()
+        {
+            string newPass;
+
+            Console.WriteLine("Enter new password: ");
+            newPass = Console.ReadLine();
+
+            string Hashedpass;
+            var sendToHashPasswordAlg = new HashingAlg(newPass);
+            Hashedpass = sendToHashPasswordAlg.getHash();
+            Hashedpass = sendToHashPasswordAlg.addSecret(Hashedpass);
+
+            connection.Open();
+            //INSERT query
+            string plainTextQuery = "UPDATE sql3346222.userCredentials SET Password=('"+Hashedpass+"')" +
+                " WHERE TrainerName = ('"+trainerName+"');";
+            //execute the query
+            MySqlCommand query = new MySqlCommand(plainTextQuery, connection);
+            MySqlDataReader rdr = query.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+            }
+            rdr.Close();
+            connection.Close();
+
+            Console.WriteLine("Password reset!");
+        }
+    }
+}
