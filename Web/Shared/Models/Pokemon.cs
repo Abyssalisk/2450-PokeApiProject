fcbd0db8ebca457902e393519b8479bda1846570
srosy <serosenvall@gmail.com>
2020-06-10 15:35:39 -0600

    controllers working in local, not in webapp yet

---
+++ Web/Shared/Models/Pokemon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Web.Shared.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
