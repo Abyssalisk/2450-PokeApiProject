3a27004192ac8f362d62605e02ba3633671d2869
srosy <serosenvall@gmail.com>
2020-06-10 19:51:35 -0600

    setup controllers and models for app, can begin transferring code from console app as desired.

---
+++ Web/Shared/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Web.Shared.Models
+{
+    class Login
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        // @Derek any other object properties you need
+    }
+}
