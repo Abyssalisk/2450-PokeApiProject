3621c593a0efd6029642820ee68efc3e2b89d00f
Samuel Gardner <ubfunkeyboy@gmail.com>
2020-06-15 03:46:14 -0600

    Cleaned up HuffmanCoder and squashed the last bug I found with extreme prejudice.

--- PokemonSimulator/Program.cs
+++ PokemonSimulator/Program.cs
@@ -20,37 +20,6 @@ namespace PokemonSimulator
     {
         static void Main(string[] args)
         {
-            //const string huffTest1 = "Lorem ipsum dolor sit amet means roughly " +
-            //    "\'Lorem very pain let it be carrots\'. Interesting, Huh? []I[]Like[]Brackets[]" +
-            //    "Very[]Much[] and you can use them as the letter O, but boxy. H[]wdy there, " +
-            //    "my []ctopus like fr[][]t l[][]ps.";
-            //const string huffTest2 = "abcabcabcaaaaabbbbb";
-            //const string huffTest3 = "[\']banana[\']aaannb[\']";
-            //const string connectionString = "";
-            //MySqlConnection server = new MySqlConnection(connectionString);
-            //server.Open();
-            //string userID = Console.ReadLine().Trim();
-            //MySqlCommand command = new MySqlCommand($"SELECT USER_ID FROM USERS WHERE USERS.USER_ID = {userID}", server);
-            //MySqlDataReader reader = command.ExecuteReader();
-            //command.CommandText = $"SELECT * {reader.GetFieldValue<int>(0)}";
-
-            //Grand.HuffmanSerialize(apip.ToString());
-
-            //JsonSerializer serializer = new JsonSerializer();
-            //serializer.NullValueHandling = NullValueHandling.Ignore;
-
-            //MemoryStream ms = new MemoryStream();
-            //using (StreamWriter sw = new StreamWriter(ms))
-            //using (BsonWriter writer = new BsonWriter(sw))
-            //{
-            //    serializer.Serialize(writer, apip);                
-            //    Console.WriteLine(ms.Length + "\n\n\n");
-            //}
-            //Console.WriteLine(apip.ToString().Length + "\n\n\n");
-            //Pokemon p = new Pokemon(apip);
-            //Console.WriteLine(p.ToString() + "\n\n\n");
-            //Grand.HuffmanSerialize("AAAAbbbCCd");
-
             bool playing = true;
             float compressionAverageEffc = 0f;
             float compressionAverageTime = 0f;
@@ -59,19 +28,12 @@ namespace PokemonSimulator
             {
                 APIPokemonBlueprint enemyApi = APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString());
                 APIPokemonBlueprint yoursApi = APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString());
-                //string result = Grand.HuffmanDeserialize(Grand.HuffmanSerialize(huffTest1));
-                //Console.WriteLine("Hufftest1: " + (huffTest1 == result));
-                //result = Grand.HuffmanDeserialize(Grand.HuffmanSerialize(huffTest2));
-                //Console.WriteLine("Hufftest2: " + (huffTest2 == result));
-                //result = Grand.HuffmanDeserialize(Grand.HuffmanSerialize(huffTest3));
-                //Console.WriteLine("Hufftest3: " + (huffTest3 == result));
-                string result = Grand.HuffmanDeserialize(Grand.HuffmanSerialize(enemyApi.ToString()));
-                //string cool = Grand.HuffmanCoder.DualNode
+                string result = Grand.HuffmanDecompress(Grand.HuffmanCompress(enemyApi.ToString()));
                 Console.WriteLine("Serialization Test Success: " + (result == enemyApi.ToString()));
                 int beforeCompression = enemyApi.ToString().Length;
                 Stopwatch sw = new Stopwatch();
                 sw.Restart();
-                string compressed = Grand.HuffmanSerialize(enemyApi.ToString());
+                string compressed = Grand.HuffmanCompress(enemyApi.ToString());
                 sw.Stop();
                 int afterCompression = compressed.Length;
                 Console.WriteLine($"{enemyApi.name} before compression is " + beforeCompression + " characters long");
--- Testing/HuffmanTest.cs
+++ Testing/HuffmanTest.cs
@@ -1,5 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PokemonSimulator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Testing
 {
@@ -9,64 +13,90 @@ namespace Testing
         [TestMethod]
         public void SampleHuffOne()
         {
-            const string testString = "abcabcabcaaaaabbbbb";
-            Assert.AreEqual(Grand.HuffmanDeserialize(Grand.HuffmanSerialize(testString)), testString);
+            const string testString = @"abcabcabcaaaaabbbbb";
+            Assert.AreEqual(testString, Grand.HuffmanDecompress(Grand.HuffmanCompress(testString)));
         }
         [TestMethod]
         public void SampleHuffTwo()
         {
-            const string testString = "[\']banana[\']aaannb[\']";
-            Assert.AreEqual(Grand.HuffmanDeserialize(Grand.HuffmanSerialize(testString)), testString);
+            const string testString = @"[']banana[']aaannb[']";
+            Assert.AreEqual(testString, Grand.HuffmanDecompress(Grand.HuffmanCompress(testString)));
         }
         [TestMethod]
         public void SampleHuffThree()
         {
-            const string testString = "Lorem ipsum dolor sit amet means roughly \'Lorem very pain let it be carrots\'. " +
+            const string testString = @"Lorem ipsum dolor sit amet means roughly 'Lorem very pain let it be carrots'. " +
                 "Interesting, Huh? []I[]Like[]Brackets[]Very[]Much[] and you can use them as the letter O, but boxy. " +
                 "H[]wdy there, my []ctopus like fr[][]t l[][]ps.";
-            Assert.AreEqual(Grand.HuffmanDeserialize(Grand.HuffmanSerialize(testString)), testString);
+            Assert.AreEqual(testString, Grand.HuffmanDecompress(Grand.HuffmanCompress(testString)));
         }
+        /// <summary>
+        /// This one is to check an edge case bug I suspect might exist.
+        /// </summary>
         [TestMethod]
         public void SampleHuffFour()
         {
-            const string testString = "aaaaaaaaaaaaaaaa0000000000000000";
-            Assert.AreEqual(Grand.HuffmanDeserialize(Grand.HuffmanSerialize(testString)), testString);
+            const string testString = "aaaaaaaaaaaaaaaa0000000000000000aaaaaaaaaaaaaaaa";
+            Assert.AreEqual(testString, Grand.HuffmanDecompress(Grand.HuffmanCompress(testString)));
         }
+        /// <summary>
+        /// Mass testing on large json objects.
+        /// </summary>
         [TestMethod]
         public void BigHuffTest()
         {
             string api = string.Empty;
             string result = string.Empty;
-            api = APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
-            result = Grand.HuffmanDeserialize(Grand.HuffmanSerialize(api));
-            Assert.AreEqual(result, api);
-            api = APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
-            result = Grand.HuffmanDeserialize(Grand.HuffmanSerialize(api));
-            Assert.AreEqual(result, api);
-            api = APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
-            result = Grand.HuffmanDeserialize(Grand.HuffmanSerialize(api));
-            Assert.AreEqual(result, api);
-            api = APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
-            result = Grand.HuffmanDeserialize(Grand.HuffmanSerialize(api));
-            Assert.AreEqual(result, api);
-            api = APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
-            result = Grand.HuffmanDeserialize(Grand.HuffmanSerialize(api));
-            Assert.AreEqual(result, api);
-            api = APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
-            result = Grand.HuffmanDeserialize(Grand.HuffmanSerialize(api));
-            Assert.AreEqual(result, api);
-            api = APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
-            result = Grand.HuffmanDeserialize(Grand.HuffmanSerialize(api));
-            Assert.AreEqual(result, api);
-            api = APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
-            result = Grand.HuffmanDeserialize(Grand.HuffmanSerialize(api));
-            Assert.AreEqual(result, api);
-            api = APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
-            result = Grand.HuffmanDeserialize(Grand.HuffmanSerialize(api));
-            Assert.AreEqual(result, api);
-            api = APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
-            result = Grand.HuffmanDeserialize(Grand.HuffmanSerialize(api));
-            Assert.AreEqual(result, api);
+            //Func<object?, string> getPoke = async (object? o) => await ((object? o) => APIPokemonBlueprint.GetPokemonBlueprint(o.ToString()).ToString());
+            //Task<string> task = new Task<string>(getPoke, Grand.rand.Next(1, 785).ToString());
+            //IEnumerable GetPokemon()
+            //{
+            //    yield return await task;
+            //}
+            //Parallel.ForEach(Enumerable.Repeat()
+            ulong charCount = 0;
+            api = @APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
+            charCount = charCount + (ulong)api.Length;
+            result = Grand.HuffmanDecompress(Grand.HuffmanCompress(api));
+            Assert.AreEqual(api, result);
+            api = @APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
+            charCount = charCount + (ulong)api.Length;
+            result = Grand.HuffmanDecompress(Grand.HuffmanCompress(api));
+            Assert.AreEqual(api, result);
+            api = @APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
+            charCount = charCount + (ulong)api.Length;
+            result = Grand.HuffmanDecompress(Grand.HuffmanCompress(api));
+            Assert.AreEqual(api, result);
+            api = @APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
+            charCount = charCount + (ulong)api.Length;
+            result = Grand.HuffmanDecompress(Grand.HuffmanCompress(api));
+            Assert.AreEqual(api, result);
+            api = @APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
+            charCount = charCount + (ulong)api.Length;
+            result = Grand.HuffmanDecompress(Grand.HuffmanCompress(api));
+            Assert.AreEqual(api, result);
+            api = @APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
+            charCount = charCount + (ulong)api.Length;
+            result = Grand.HuffmanDecompress(Grand.HuffmanCompress(api));
+            Assert.AreEqual(api, result);
+            api = @APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
+            charCount = charCount + (ulong)api.Length;
+            result = Grand.HuffmanDecompress(Grand.HuffmanCompress(api));
+            Assert.AreEqual(api, result);
+            api = @APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
+            charCount = charCount + (ulong)api.Length;
+            result = Grand.HuffmanDecompress(Grand.HuffmanCompress(api));
+            Assert.AreEqual(api, result);
+            api = @APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
+            charCount = charCount + (ulong)api.Length;
+            result = Grand.HuffmanDecompress(Grand.HuffmanCompress(api));
+            Assert.AreEqual(api, result);
+            api = @APIPokemonBlueprint.GetPokemonBlueprint(Grand.rand.Next(1, 785).ToString()).ToString();
+            charCount = charCount + (ulong)api.Length;
+            result = Grand.HuffmanDecompress(Grand.HuffmanCompress(api));
+            Assert.AreEqual(api, result);
+            ulong mbs = charCount / 524288;
+            ;
         }
     }
 }
