8ec8ad3a753a40004fe7790409546c520a4103df
DrechslerDerek <66096364+DrechslerDerek@users.noreply.github.com>
2020-06-24 19:02:25 -0600

    Update Program.cs

--- PokemonSimulator/Program.cs
+++ PokemonSimulator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using PokeAPI;
@@ -8,53 +8,72 @@ namespace PokemonSimulator
 {
     public class Program
     {
+        [Obsolete]
         static void Main(string[] args)
         {
-            UserAuthAndLogin loginStart = new UserAuthAndLogin();
+            var loginStart = new UserAuthAndLogin();
             System.GC.Collect();
-
             Trainer CurrentTrainer = new Trainer();
             CurrentTrainer.UserId = loginStart.UserID;
             CurrentTrainer.TrainerName = loginStart.TrainerName;
 
-            var Lineup = new TrainerLineUp(CurrentTrainer.UserId, CurrentTrainer.TrainerName, loginStart.Connection.myConnection);
-
-
-            while (lineup.LoopStuck == true)
+            Boolean Win;
+            Boolean Exit = false;
+            while (Exit == false)
             {
-                Console.WriteLine("Would you like to leave? type 'exit'\nOr press any key to continue");
-                string exitChoice = Console.ReadLine();
 
-                if (exitChoice.ToLower().Equals("exit"))
+                var Lineup = new TrainerLineUp(CurrentTrainer.UserId, CurrentTrainer.TrainerName, loginStart.Connection.myConnection);
+
+                while (Lineup.LoopStuck == true)
                 {
-                    System.Environment.Exit(0);
+                    Console.WriteLine("Would you like to leave? type 'exit'\nOr press any key to continue");
+                    string exitChoice = Console.ReadLine();
+
+                    if (exitChoice.ToLower().Equals("exit"))
+                    {
+                        System.Environment.Exit(0);
+                    }
+                    else
+                    {
+                        loginStart = new UserAuthAndLogin();
+                        System.GC.Collect();
+                        CurrentTrainer = new Trainer();
+                        CurrentTrainer.UserId = loginStart.UserID;
+                        CurrentTrainer.TrainerName = loginStart.TrainerName;
+
+                        Lineup = new TrainerLineUp(CurrentTrainer.UserId, CurrentTrainer.TrainerName, loginStart.Connection.myConnection);
+                    }
                 }
-                else
-                {
-                    loginStart = new UserAuthAndLogin();
-                    System.GC.Collect();
-                    currentTrainer = new Trainer();
-                    currentTrainer.UserId = loginStart.UserID;
-                    currentTrainer.TrainerName = loginStart.TrainerName;
 
-                    lineup = new TrainerLineUp(currentTrainer.UserId, currentTrainer.TrainerName, loginStart.Connection.myConnection);
+                CurrentTrainer = Lineup.GhostTrainer;
+                System.GC.Collect();
+
+                Console.WriteLine("Let's Battle! ");
+
+                for(int j = 5;j>0;j--)
+                {
+                    Trainer EliteTrainer = new Trainer();
+                    LoadOpponent EliteLoader = new LoadOpponent(j, loginStart.Connection.myConnection);
+
+                    EliteTrainer.Pokemon = EliteLoader.OpponentLineUp;
+                    EliteTrainer.TrainerName = EliteLoader.OpponentName;
+
+                    Win = GameMockup.GameEngine(CurrentTrainer, EliteTrainer);
+
+                    if (Win == false)
+                    {
+                        ScoreBoard ScoreAndRanking = new ScoreBoard(Win);
+                        //this need to update db with new elite trainer if wins > 0
+
+                        //play again or exit class
+                        //if play again than make new lineup y/n?
+                        //If they beat the champion add some BS celebration stuff
+                    }
+                    System.GC.Collect();
                 }
             }
-            currentTrainer = lineup.GhostTrainer;
-            Trainer dupe = new Trainer(currentTrainer);
-            System.GC.Collect();
-
-
-            Console.WriteLine("Let's Battle! ");
-            Trainer EliteTrainerOne = new Trainer();
-            LoadOpponent EliteLoader = new LoadOpponent(1,loginStart.Connection.myConnection);
-
-            EliteTrainerOne.Pokemon = EliteLoader.OpponentLineUp;
-            EliteTrainerOne.TrainerName = EliteLoader.OpponentName;
-
-            GameMockup.GameEngine(CurrentTrainer,EliteTrainerOne);
+            System.Environment.Exit(0);
         }
        
-
     }
 }
